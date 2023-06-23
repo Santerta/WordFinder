2970edc55b39913730e9056d1cab6c42a2cf9837
santerta <santerta@gmail.com>
2023-06-23 22:38:58 +0300

    Create initial method to print found words
    Create initial console-program that asks for input and print the results to console.

--- src/wordFinder/WordFinder.java
+++ src/wordFinder/WordFinder.java
@@ -15,21 +15,16 @@ import java.util.Scanner;
  */
 public class WordFinder {
     
-    // program is given a list of 9 different letters
-    // program goes through an english dictionary and checks for every word that if it can be written with those letters
-    //              if the word can be written, program adds it to a collection
-    // lastly the program prints all the words that can be written from 9 letters to 3 letters
-    int rowCount;
-    ArrayList<List<String>> validWords = new ArrayList<>();
+    private ArrayList<List<String>> validWords = new ArrayList<>();
     
-    String wordsFilePath = "filtered_words.txt";
+    private String wordsFilePath = "filtered_words.txt";
     
+    private static Scanner userInput = new Scanner(System.in);
     
     /**
      * @param howManyLetters asd
      */
     public WordFinder(int howManyLetters) {
-        this.rowCount = howManyLetters;
         this.validWords = new ArrayList<>();
         for (int i = 0; i < howManyLetters ; i++ ) {
             this.validWords.add(new ArrayList<>());
@@ -40,18 +35,136 @@ public class WordFinder {
      * @param args not in use
      */
     public static void main(String[] args) {
-        WordFinder helper = new WordFinder(9);
+        /*
+        WordFinder finder = new WordFinder(9);
         
         String testLetters = "sginadtse";
-        helper.findAllTheWords(testLetters);
+        finder.findAllTheWords(testLetters);
+        finder.printFoundWords(3, 9, 10);
         
-        for (int i = 0 ; i < helper.validWords.size() ; i++) {
-            int howManyWords = helper.validWords.get(i).size();
+        
+        for (int i = 2 ; i < finder.validWords.size() ; i++) {
+            int howManyWords = finder.validWords.get(i).size();
             System.out.println("Number of " + (i+1) + " letter words: " + howManyWords);
         }
+        */
         
+        startProgram();
         
     }
+    
+    private static void startProgram() {
+        WordFinder finder = new WordFinder(9);
+        finder.startMenu();
+        
+    }
+    
+    
+    private void startMenu() {
+        System.out.println("WORDFINDER");
+        System.out.println("==================");
+        System.out.println("Which round do you need help with?\n"
+                + "1. Letters round\n"
+                + "2. Numbers round\n"
+                + "3. Conundrum\n"
+                + "4. Exit program"
+                );
+        
+        int pressedButton = userInput.nextInt();
+        
+        if (pressedButton == 1) {
+            startLettersRound();
+        } else if (pressedButton == 2) {
+            startNumbersRound();
+        } else if (pressedButton == 3) {
+            startConundrumRound();
+        } else if (pressedButton == 4) {
+            System.out.println("Exiting program\n");
+        } else {
+            System.out.println("Please provide a number between 1-4.\n");
+            startMenu();
+        }
+    }
+    
+    
+    private void startConundrumRound() {
+        System.out.println("Conundrum round helper still under construction.");
+        System.out.println("1. Back to start menu\n"
+                + "2. Exit program");
+        int pressedButton = userInput.nextInt();
+        if (pressedButton == 1) {
+            startMenu();
+        } else if (pressedButton == 2) {
+            System.out.println("Bye bye");
+        } else {
+            System.out.println("Please provide a number between 1-2.");
+            startConundrumRound();
+        }
+        
+
+    }
+    
+    
+    private void startNumbersRound() {
+        System.out.println("Numbers round helper still under construction.");
+        System.out.println("1. Back to start menu\n"
+                + "2. Exit program");
+        int pressedButton = userInput.nextInt();
+        if (pressedButton == 1) {
+            startMenu();
+        } else if (pressedButton == 2) {
+            System.out.println("Bye bye");
+        } else {
+            System.out.println("Please provide a number between 1-2.");
+            startNumbersRound();
+        }
+    }
+    
+    
+    private void printFoundWords(int minimumLetters, int maximumLetters, int maxWords) {
+        
+        for (int i = maximumLetters ; i > minimumLetters ; i--) {
+            int currentMax = maxWords;
+            if ( this.validWords.get(i-1).size() < maxWords) currentMax = this.validWords.get(i-1).size();
+            
+            System.out.println("-----------------------");
+            System.out.println((i) + " letter words:");
+            for (int j = 0  ; j < currentMax ; j++) {
+                System.out.println(this.validWords.get(i-1).get(j));
+            }
+        }
+        System.out.println("-----------------------");
+        
+    }
+    
+    
+    private void startLettersRound() {
+        int minimum = 3;
+        int maximum = 9;
+        int howManyWords = 10;
+        String examinedLetters;
+        
+        System.out.println("LETTERS ROUND HELPER\n-------------------\n");
+        System.out.println("Please enter the nine letters:");
+        
+        examinedLetters = userInput.nextLine();
+        
+        if (examinedLetters.length() != 9) {
+            System.out.println("Please give exactly 9 letters.");
+            startLettersRound();
+        }
+        
+        findAllTheWords(examinedLetters);
+        printFoundWords(minimum, maximum, howManyWords);
+        
+        System.out.println("1. Start again\n" + "2. Exit program");
+        int pressedButton = userInput.nextInt();
+        
+        if (pressedButton == 1) {
+            startMenu();
+        }
+    }
+    
     
     private void findAllTheWords(String givenLetters) {
 
@@ -89,7 +202,7 @@ public class WordFinder {
     
     @SuppressWarnings("unused")
     private void filterWordsToAFile() {
-        String filePath = "./words_alpha.txt"; // Replace with the actual file path
+        String filePath = "./words_alpha.txt";
         String outputFile = "filtered_words.txt";
         
         try ( Scanner fi = new Scanner(new FileInputStream(filePath));
